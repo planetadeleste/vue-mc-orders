520d42865cdb058315917fd2c97a3c7312d4dac0
Alvaro Canepa <bfpdevel@gmail.com>
2022-12-11 06:29:00 -0300

    - Fix types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,138 +1,165 @@
-import "@planetadeleste/vue-mc";
-import "@planetadeleste/vue-mc-shopaholic";
-import { RequestOptions } from "vue-mc";
-import {
-  CartComponentAddData,
-  CartComponentAddItemData,
-  CartComponentItemsData,
-  CartComponentPositionData,
-  CartComponentRemoveData,
-  CartData,
-  CartPositionData,
-  ItemPriceData,
-  MakeOrderResponseData,
-  OrderData,
-  OrderPositionData,
-  OrderRequestData,
-  OrderRequestOrderData,
-  OrderRequestUserData,
-  OrderStatsResponse,
-  PaymentMethodData,
-  StatusData,
-  TotalPriceContainerData,
-} from "./types";
-
 declare module "@planetadeleste/vue-mc-orders" {
-  import { StatResponse } from "@planetadeleste/vue-mc-shopaholic";
-  import { Model, Collection } from "@planetadeleste/vue-mc";
-  import { Response } from "vue-mc";
-  export type ResponseResult<T> = Response<T>;
+    import "@planetadeleste/vue-mc";
+    import "@planetadeleste/vue-mc-shopaholic";
+    import {StatResponse} from "@planetadeleste/vue-mc-shopaholic";
+    import {Collection, Model} from "@planetadeleste/vue-mc";
+    import {RequestOptions, Response} from "vue-mc";
 
-  export {
-    CartComponentAddData,
-    CartComponentAddItemData,
-    CartComponentItemsData,
-    CartComponentPositionData,
-    CartComponentRemoveData,
-    CartData,
-    CartPositionData,
-    ItemPriceData,
-    MakeOrderResponseData,
-    OrderData,
-    OrderPositionData,
-    OrderRequestData,
-    OrderRequestOrderData,
-    OrderRequestUserData,
-    OrderStatsResponse,
-    PaymentMethodData,
-    StatusData,
-    TotalPriceContainerData,
-  };
+    type CartComponentAddData = import("./types/Cart").CartComponentAddData;
+    type CartComponentAddItemData = import("./types/Cart").CartComponentAddItemData;
+    type CartComponentItemsData = import("./types/Cart").CartComponentItemsData;
+    type CartComponentPositionData = import("./types/Cart").CartComponentPositionData;
+    type CartComponentRemoveData = import("./types/Cart").CartComponentRemoveData;
+    type CartData = import("./types/Cart").CartData;
+    type CartPositionData = import("./types/Cart").CartPositionData;
+    type ItemPriceData = import("./types/Cart").ItemPriceData;
+    type MakeOrderResponseData = import("./types/Order").MakeOrderResponseData;
+    type OrderData = import("./types/Order").OrderData;
+    type OrderPositionData = import("./types/OrderPosition").OrderPositionData;
+    type OrderRequestData = import("./types/Order").OrderRequestData;
+    type OrderRequestOrderData = import("./types/Order").OrderRequestOrderData;
+    type OrderRequestUserData = import("./types/Order").OrderRequestUserData;
+    type OrderStatsResponse = import("./types/OrderStatsResponse").OrderStatsResponse;
+    type PaymentMethodData = import("./types/PaymentMethod").PaymentMethodData;
+    type StatusData = import("./types/Status").StatusData;
+    type TotalPriceContainerData = import("./types/Cart").TotalPriceContainerData;
 
-  interface Cart extends Model, CartData {}
-  class Cart extends Model {
-    getData(): Promise<ResponseResult<CartData>>;
+    export type ResponseResult<T> = Response<T>;
 
-    /**
-     * @description Add offers to cart
-     * @author Alvaro Canepa <bfpdevel@gmail.com>
-     * @param {CartComponentAddData} obData
-     * @return {*}  {Promise<ResponseResult<CartComponentItemsData>>}
-     * @memberof Cart
-     */
-    add(
-      obData: CartComponentAddData
-    ): Promise<ResponseResult<CartComponentItemsData>>;
-    add(
-      obData: CartComponentAddData,
-      bReturnData: true
-    ): Promise<ResponseResult<CartData>>;
+    interface Cart extends Model, CartData {
+    }
 
-    /**
-     * @description Updates quantity of offers in cart
-     * @author Alvaro Canepa <bfpdevel@gmail.com>
-     * @param {CartComponentAddData} obData
-     * @param {Boolean} bReturnData
-     * @return {*}  {GetCartResponseType<T>}
-     * @memberof Cart
-     */
-    update(
-      obData: CartComponentAddData
-    ): Promise<ResponseResult<CartComponentItemsData>>;
-    update(
-      obData: CartComponentAddData,
-      bReturnData: true
-    ): Promise<ResponseResult<CartData>>;
+    class Cart extends Model {
+        getData(): Promise<ResponseResult<CartData>>;
 
-    /**
-     * @description Removes positions from cart by offer ID or cart position ID
-     * @author Alvaro Canepa <bfpdevel@gmail.com>
-     * @param {CartComponentRemoveData} obData
-     * @param {Boolean} bReturnData
-     * @return {*}  {GetCartResponseType<T>}
-     * @memberof Cart
-     */
-    remove(
-      obData: CartComponentRemoveData
-    ): Promise<ResponseResult<CartComponentItemsData>>;
-    remove(
-      obData: CartComponentRemoveData,
-      bReturnData: true
-    ): Promise<ResponseResult<CartData>>;
+        /**
+         * @description Add offers to cart
+         * @author Alvaro Canepa <bfpdevel@gmail.com>
+         * @param {CartComponentAddData} obData
+         * @return {*}  {Promise<ResponseResult<CartComponentItemsData>>}
+         * @memberof Cart
+         */
+        add(
+            obData: CartComponentAddData
+        ): Promise<ResponseResult<CartComponentItemsData>>;
+        add(
+            obData: CartComponentAddData,
+            bReturnData: true
+        ): Promise<ResponseResult<CartData>>;
 
-    /**
-     * @description Get cart items
-     * @author Alvaro Canepa <bfpdevel@gmail.com>
-     * @return {*}  {Promise<ResponseResults<CartComponentItemsData>>}
-     * @memberof Cart
-     */
-    getItems(): Promise<ResponseResult<CartComponentItemsData>>;
-  }
+        /**
+         * @description Updates quantity of offers in cart
+         * @author Alvaro Canepa <bfpdevel@gmail.com>
+         * @param {CartComponentAddData} obData
+         * @param {Boolean} bReturnData
+         * @return {*}  {GetCartResponseType<T>}
+         * @memberof Cart
+         */
+        update(
+            obData: CartComponentAddData
+        ): Promise<ResponseResult<CartComponentItemsData>>;
+        update(
+            obData: CartComponentAddData,
+            bReturnData: true
+        ): Promise<ResponseResult<CartData>>;
 
-  interface Order extends Model, OrderData {}
-  class Order extends Model {
-    loadPosition(): Promise<void>;
-    store(
-      options?: RequestOptions
-    ): Promise<Response<MakeOrderResponseData | any> | null>;
-    stats(): Promise<Response<OrderStatsResponse>>;
-  }
-  export class Orders extends Collection<Order> {}
+        /**
+         * @description Removes positions from cart by offer ID or cart position ID
+         * @author Alvaro Canepa <bfpdevel@gmail.com>
+         * @param {CartComponentRemoveData} obData
+         * @param {Boolean} bReturnData
+         * @return {*}  {GetCartResponseType<T>}
+         * @memberof Cart
+         */
+        remove(
+            obData: CartComponentRemoveData
+        ): Promise<ResponseResult<CartComponentItemsData>>;
+        remove(
+            obData: CartComponentRemoveData,
+            bReturnData: true
+        ): Promise<ResponseResult<CartData>>;
 
-  interface PaymentMethod extends Model, PaymentMethodData {}
-  class PaymentMethod extends Model {}
-  export class PaymentMethods extends Collection<PaymentMethod> {}
+        /**
+         * @description Get cart items
+         * @author Alvaro Canepa <bfpdevel@gmail.com>
+         * @return {*}  {Promise<ResponseResults<CartComponentItemsData>>}
+         * @memberof Cart
+         */
+        getItems(): Promise<ResponseResult<CartComponentItemsData>>;
+    }
 
-  interface Status extends Model, StatusData {}
-  class Status extends Model {}
-  export class Statuses extends Collection<Status> {}
+    interface Order extends Model, OrderData {
+    }
 
-  interface OrderPosition extends Model, OrderPositionData {}
-  class OrderPosition extends Model {}
-  export class OrderPositions extends Collection<OrderPosition> {
-    id: number;
-    secret_key: string;
-  }
+    class Order extends Model {
+        loadPosition(): Promise<void>;
 
-  export { Cart, Order, OrderPosition, PaymentMethod, Status };
+        store(
+            options?: RequestOptions
+        ): Promise<Response<MakeOrderResponseData | any> | null>;
+
+        stats(): Promise<Response<OrderStatsResponse>>;
+    }
+
+    class Orders extends Collection<Order> {
+    }
+
+    interface PaymentMethod extends Model, PaymentMethodData {
+    }
+
+    class PaymentMethod extends Model {
+    }
+
+    class PaymentMethods extends Collection<PaymentMethod> {
+    }
+
+    interface Status extends Model, StatusData {
+    }
+
+    class Status extends Model {
+    }
+
+    class Statuses extends Collection<Status> {
+    }
+
+    interface OrderPosition extends Model, OrderPositionData {
+    }
+
+    class OrderPosition extends Model {
+    }
+
+    class OrderPositions extends Collection<OrderPosition> {
+        id: number;
+        secret_key: string;
+    }
+
+    export {
+        Cart,
+        CartComponentAddData,
+        CartComponentAddItemData,
+        CartComponentItemsData,
+        CartComponentPositionData,
+        CartComponentRemoveData,
+        CartData,
+        CartPositionData,
+        ItemPriceData,
+        MakeOrderResponseData,
+        Order,
+        Orders,
+        OrderData,
+        OrderPosition,
+        OrderPositions,
+        OrderPositionData,
+        OrderRequestData,
+        OrderRequestOrderData,
+        OrderRequestUserData,
+        OrderStatsResponse,
+        PaymentMethod,
+        PaymentMethods,
+        PaymentMethodData,
+        Status,
+        Statuses,
+        StatusData,
+        TotalPriceContainerData,
+    };
 }
